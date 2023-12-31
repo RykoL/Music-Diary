1c67957ff8f15335306e5844c6c03c146b79ad45
Rico Lang <rico.lang@thoughtworks.com>
2023-12-31 15:49:07 +0100

    Add welcome page

--- src/routes/diary/+layout.svelte
+++ src/routes/diary/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import '../app.css';
     import NewEntryForm from "$lib/components/NewEntryForm.svelte";
+
     let dialog: HTMLDialogElement;
 
     const showModal = () => {
@@ -9,14 +10,13 @@
 </script>
 
 
-<div class="navbar bg-base-100 border-b-2 mb-4">
+<div class="navbar bg-base-100 border-b-2">
     <div class="flex-1 gap-1">
         <a class="btn btn-ghost normal-case text-xl" href="/diary">My music diary</a>
-        <a href="/diary">Home</a>
     </div>
     <button class="btn btn-circle btn-primary btn-md" aria-label="Add new entry" on:click={showModal}>+</button>
 </div>
-
+<slot/>
 <dialog id="new-entry-modal" class="modal" bind:this={dialog}>
     <div class="modal-box">
         <form method="dialog">
@@ -25,4 +25,3 @@
         <NewEntryForm method="post"/>
     </div>
 </dialog>
-<slot/>
