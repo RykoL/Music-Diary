763191f5427a71c7eeb1d89545f21f0dc08e98e8
Rico Lang <rico.lang@thoughtworks.com>
2024-01-12 13:49:51 +0100

    Make db connection persistent

--- src/lib/server/infrastructure/DatabaseFactory.ts
+++ src/lib/server/infrastructure/DatabaseFactory.ts
@@ -6,13 +6,18 @@ export class DatabaseFactory {
 
     private static dbName: string = "diary.sqlite3";
     private static isMigrated: boolean = false;
+    static dbInstance: Database<sqlite3.Database, Statement> | null = null;
 
     static async connect(): Promise<Database<sqlite3.Database, Statement>> {
+        if (this.dbInstance) {
+            return this.dbInstance
+        }
         const dbNameToUse = env.NODE_ENV === 'test' ? ':memory:' : this.dbName
         const db = await open({filename: dbNameToUse, driver: sqlite3.cached.Database})
         await db.migrate({
             migrationsPath: "./migrations/"
         })
+        this.dbInstance = db
         return db
     }
 
