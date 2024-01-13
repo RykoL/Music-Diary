9e6a6c640e0e2a1aa37be19573c3a615f71bbf14
Rico Lang <rico.lang@thoughtworks.com>
2024-01-13 11:11:42 +0100

    Make e2e tests more reliable when run in paralell

--- e2e/diary.spec.ts
+++ e2e/diary.spec.ts
@@ -1,14 +1,19 @@
 import {test, expect} from '@playwright/test';
 
-test("create a diary", async ( {page}) => {
+test("create a diary", async ({page}) => {
+    const random = Math.random()
+    const title = `Test diary ${random}`
+    const description = `Diary used for testing ${random}`
     await page.goto("/diaries")
 
     await page.getByRole('button', {name: 'Start new diary'}).click()
 
-    await page.getByLabel('Title').fill('Test diary');
-    await page.getByLabel('Description').fill('Diary used for testing');
+    await page.getByLabel('Title').fill(title);
+    await page.getByLabel('Description').fill(description);
     await page.getByRole('button', {name: 'Create'}).click()
 
-    await expect(page.getByText('Test diary')).toBeVisible({timeout: 5000})
-    await expect(page.getByText('Diary used for testing')).toBeVisible()
+    await expect(page.getByText(title)).toBeVisible({timeout: 5000})
+    await expect(page.getByText(description)).toBeVisible()
+
+    await page.getByRole('button', {name: title}).click()
 })
---
+++ src/lib/components/DiaryCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type {DiaryPresentation} from "$lib/models/DiaryPresentation";
+
+    export let diary: DiaryPresentation
+</script>
+<div class="card w-96 bg-base-100 shadow-xl image-full mx-auto">
+    <figure><img src="https://picsum.photos/400/200" alt="diary"/></figure>
+    <div class="w-full flex flex-row justify-end h-fit pt-2 px-2">
+        <form action="?/delete" method="post" class="z-50">
+            <input type="hidden" name="diaryId" value={diary.id}>
+            <button class="btn btn-sm btn-ghost" aria-label="Remove {diary.title}">x</button>
+        </form>
+    </div>
+    <div class="card-body">
+        <h2 class="card-title">{diary.title}</h2>
+        <p>{diary.description}</p>
+        <div class="card-actions justify-end">
+            <a href="/diary/{diary.id}" class="btn btn-primary">Open Now</a>
+        </div>
+    </div>
+</div>
