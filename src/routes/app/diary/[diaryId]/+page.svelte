7b3437fe7cc5ea1be770ee7f63c416c8566df2e3
Rico Lang <rico.lang@thoughtworks.com>
2024-01-19 16:20:50 +0100

    Introduce back button on diary page

--- src/routes/app/diary/[diaryId]/+page.svelte
+++ src/routes/app/diary/[diaryId]/+page.svelte
@@ -3,6 +3,7 @@
     import EntryCard from "$lib/components/EntryCard.svelte";
     import NewEntryForm from "$lib/components/NewEntryForm.svelte";
     import Navbar from "$lib/components/Navbar.svelte";
+    import Link from "$lib/components/Link.svelte";
 
     let dialog: HTMLDialogElement;
 
@@ -12,6 +13,7 @@
     export let data: PageData;
 </script>
 <Navbar>
+    <Link href="/app/diaries">Back</Link>
     <a class="btn btn-ghost normal-case text-xl col-start-2" href="/app/diaries">My music diary</a>
     <div class="flex justify-end col-start-3">
         <button class="btn btn-circle btn-primary btn-md" aria-label="Write new entry" on:click={showModal}>
@@ -20,6 +22,11 @@
     </div>
 </Navbar>
 <section class="container flex flex-col gap-5 mt-2">
+    {#if data.entries.length === 0}
+        <div class="prose">
+            <p>It looks pretty empty in here. How about you create a new entry.</p>
+        </div>
+    {/if}
     {#each data.entries as entry}
         <EntryCard entry={entry} diaryId="{data.id}"/>
     {/each}
